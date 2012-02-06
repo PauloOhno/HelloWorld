92fa6778288bf2eb945e41447529c2572f3492de
pauloohno <pauloohno@gmail.com>
2012-02-05 20:12:32 -0500

    Teste de commit no forked repository e pull request.
    Signed-off-by: pauloohno <pauloohno@gmail.com>

--- HelloWorld/src/net/studyprojects/main/HelloWorldTest.java
+++ HelloWorld/src/net/studyprojects/main/HelloWorldTest.java
@@ -1,5 +1,10 @@
 package net.studyprojects.main;
 
+/**
+ * Teste de commit no repositorio forked e pedido de pull request.
+ * @author Paul
+ *
+ */
 public class HelloWorldTest {
 
 	public static void main(String[] args) {
